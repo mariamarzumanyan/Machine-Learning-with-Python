be5a80b41c657de8f2bfe77de060d7dd33732e83
Mariam Arzumanyan <arzumanyan.mariam.cba@gmail.com>
2021-10-06 15:42:07 -0500

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -14,11 +14,6 @@ import matplotlib.pyplot as plt
 from scipy.io import loadmat
 import time
 
-%matplotlib notebook
-
-from helper import *
-
-print('You\'re running python %s' % sys.version.split(' ')[0])
 
 data = loadmat("ion.mat")
 xTr  = data['xTr'].T
@@ -85,44 +80,6 @@ def grid_search(xTr, yTr, xVal, yVal, depths):
     
     return best_depth, training_losses, validation_losses
 
-# The following tests check that your implementation of grid search returns the correct number of training and validation loss values and the correct best depth
-
-depths = [1,2,3,4,5]
-k = len(depths)
-best_depth, training_losses, validation_losses = grid_search(xTr, yTr, xTe, yTe, depths)
-best_depth_grader, training_losses_grader, validation_losses_grader = grid_search_grader(xTr, yTr, xTe, yTe, depths)
-
-# Check the length of the training loss
-def grid_search_test1():
-    return (len(training_losses) == k) 
-
-# Check the length of the validation loss
-def grid_search_test2():
-    return (len(validation_losses) == k)
-
-# Check the argmin
-def grid_search_test3():
-    return (best_depth == depths[np.argmin(validation_losses)])
-
-def grid_search_test4():
-    return (best_depth == best_depth_grader)
-
-def grid_search_test5():
-    return np.linalg.norm(np.array(training_losses) - np.array(training_losses_grader)) < 1e-7
-
-def grid_search_test6():
-    return np.linalg.norm(np.array(validation_losses) - np.array(validation_losses_grader)) < 1e-7
-
-runtest(grid_search_test1, 'grid_search_test1')
-runtest(grid_search_test2, 'grid_search_test2')
-runtest(grid_search_test3, 'grid_search_test3')
-runtest(grid_search_test4, 'grid_search_test4')
-runtest(grid_search_test5, 'grid_search_test5')
-runtest(grid_search_test6, 'grid_search_test6')
-
-
-#import random
-#from random import randrange
 
 def generate_kFold(n, k):
     '''
@@ -166,46 +123,6 @@ def generate_kFold(n, k):
 
 generate_kFold(3,3)
 
-# The following tests check that your generate_kFold function 
-# returns the correct number of total indices, 
-# train and test indices, and the correct ratio
-
-kfold_indices = generate_kFold(1004, 5)
-
-def generate_kFold_test1():
-    return len(kfold_indices) == 5 # you should generate 5 folds
-
-def generate_kFold_test2():
-    t = [((len(train_indices) + len(test_indices)) == 1004) 
-         for (train_indices, test_indices) in kfold_indices]
-    return np.all(t) # make sure that both for each fold, the number of examples sum up to 1004
-
-def generate_kFold_test3():
-    ratio_test = []
-    for (train_indices, validation_indices) in kfold_indices:
-        ratio = len(validation_indices) / len(train_indices)
-        ratio_test.append((ratio > 0.24 and ratio < 0.26))
-    # make sure that both for each fold, the training to validation 
-    # examples ratio is in between 0.24 and 0.25
-    return np.all(ratio_test) 
-
-def generate_kFold_test4():
-    train_indices_set = set() # to keep track of training indices for each fold
-    validation_indices_set = set() # to keep track of validation indices for each fold
-    for (train_indices, validation_indices) in kfold_indices:
-        train_indices_set = train_indices_set.union(set(train_indices))
-        validation_indices_set = validation_indices_set.union(set(validation_indices))
-    
-    # Make sure that you use all the examples in all the training fold and validation fold
-    return train_indices_set == set(np.arange(1004)) and validation_indices_set == set(np.arange(1004))
-
-
-runtest(generate_kFold_test1, 'generate_kFold_test1')
-runtest(generate_kFold_test2, 'generate_kFold_test2')
-runtest(generate_kFold_test3, 'generate_kFold_test3')
-runtest(generate_kFold_test4, 'generate_kFold_test4')
-
-
 def cross_validation(xTr, yTr, depths, indices):
     '''
     Input:
@@ -245,43 +162,5 @@ Part 3 : Find the best depth of tree using cross validation and grid search (cro
     
     return best_depth, training_losses, validation_losses
 
-# The following tests check that your implementation of cross_validation returns the correct number of training and validation losses, the correct "best depth" and the correct values for training and validation loss
-
-depths = [1,2,3,4]
-k = len(depths)
-
-# generate indices
-# the same indices will be used to cross check your solution and ours
-indices = generate_kFold(len(xTr), 5)
-best_depth, training_losses, validation_losses = cross_validation(xTr, yTr, depths, indices)
-best_depth_grader, training_losses_grader, validation_losses_grader = cross_validation_grader(xTr, yTr, depths, indices)
-
-# Check the length of the training loss
-def cross_validation_test1():
-    return (len(training_losses) == k) 
-
-# Check the length of the validation loss
-def cross_validation_test2():
-    return (len(validation_losses) == k)
-
-# Check the argmin
-def cross_validation_test3():
-    return (best_depth == depths[np.argmin(validation_losses)])
-
-def cross_validation_test4():
-    return (best_depth == best_depth_grader)
-
-def cross_validation_test5():
-    return np.linalg.norm(np.array(training_losses) - np.array(training_losses_grader)) < 1e-7
-
-def cross_validation_test6():
-    return np.linalg.norm(np.array(validation_losses) - np.array(validation_losses_grader)) < 1e-7
-
-runtest(cross_validation_test1, 'cross_validation_test1')
-runtest(cross_validation_test2, 'cross_validation_test2')
-runtest(cross_validation_test3, 'cross_validation_test3')
-runtest(cross_validation_test4, 'cross_validation_test4')
-runtest(cross_validation_test5, 'cross_validation_test5')
-runtest(cross_validation_test6, 'cross_validation_test6')
 
 
