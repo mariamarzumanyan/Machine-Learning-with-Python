d2c6a8cc108be7863fd60b187a7e2777727e16e2
Mariam Arzumanyan <arzumanyan.mariam.cba@gmail.com>
2021-10-06 15:40:17 -0500

    Update gradient_boosting_regression_trees.py

--- gradient_boosting_regression_trees.py
+++ gradient_boosting_regression_trees.py
@@ -14,18 +14,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 import time
-from helper import *
-%matplotlib notebook
-
-print('You\'re running python %s' % sys.version.split(' ')[0])
 
 
 xTrSpiral,yTrSpiral,xTeSpiral,yTeSpiral= spiraldata(150)
 xTrIon,yTrIon,xTeIon,yTeIon= iondata()
 
 
-# Create a regression tree with depth 4
-tree = RegressionTree(depth=4)
+# Create a regression tree with depth 6
+tree = RegressionTree(depth=6)
 
 # To fit/train the regression tree
 tree.fit(xTrSpiral, yTrSpiral)
@@ -123,65 +119,6 @@ def evalboostforest(trees, X, alphas=None):
     
     return pred
 
-def evalboostforest_test0():
-    m = 200
-    x = np.arange(100).reshape((100, 1))
-    y = np.arange(100)
-    trees = forest(x, y, m) # create a list of trees 
-    preds = evalboostforest(trees, x)
-    return preds.shape == y.shape
-
-def evalboostforest_test1():
-    m = 200
-    x = np.random.rand(10,3)
-    y = np.ones(10)
-    x2 = np.random.rand(10,3)
-
-    max_depth = 0
-    
-    # Create a forest with trees depth 0
-    # Since the data are all ones, each tree will return 1 as prediction
-    trees = forest(x, y, m, max_depth) # create a list of trees      
-    pred = evalboostforest(trees, x2)[0]
-    return np.isclose(pred,1)  # the prediction should be equal to the sum of weights
-
-def evalboostforest_test2(): 
-    # results should match evalforest if alphas are 1/m and labels +1, -1
-    m = 20
-    x = np.arange(100).reshape((100, 1))
-    y = np.sign(np.arange(100))
-    trees = forest(x, y, m) # create a list of m trees 
-
-    alphas=np.ones(m)/m
-    preds1 = evalforest(trees, x) #evaluate the forest using our own implementation
-    preds2 = evalboostforest(trees, x, alphas)
-    return np.all(np.isclose(preds1,preds2))
-
-def evalboostforest_test3(): 
-    # if only alpha[i]=1 and all others are 0, the result should match exactly 
-    # the predictions of the ith tree
-    m = 20
-    x = np.random.rand(100,5)
-    y = np.arange(100)
-    x2 = np.random.rand(20,5)
-
-    trees = forest(x, y, m)  # create a list of m trees
-    allmatch=True
-    for i in range(m): # go through each tree i
-        alphas=np.zeros(m)
-        alphas[i]=1.0; # set only alpha[i]=1 all other alpha=0
-        preds1 = trees[i].predict(x2) # get prediction of ith tree
-        preds2 = evalboostforest(trees, x2, alphas) # get prediction of weighted ensemble
-        allmatch=allmatch and all(np.isclose(preds1,preds2))
-    return allmatch
-
-
-# and some new tests to check if the weights (and the np.sign function) were incorporated correctly 
-runtest(evalboostforest_test0, 'evalboostforest_test0')
-runtest(evalboostforest_test1, 'evalboostforest_test1')
-runtest(evalboostforest_test2, 'evalboostforest_test2')
-runtest(evalboostforest_test3, 'evalboostforest_test3')
-
 
 
 def GBRT(xTr, yTr, m, maxdepth=4, alpha=0.1):
@@ -218,43 +155,6 @@ def GBRT(xTr, yTr, m, maxdepth=4, alpha=0.1):
        
     return trees, weights
 
-trees, weights = GBRT(xTrSpiral,yTrSpiral, 50)
-
-def GBRT_test1():
-    m = 40
-    x = np.arange(100).reshape((100, 1))
-    y = np.arange(100)
-    trees, weights = GBRT(x, y, m, alpha=0.1)
-    return len(trees) == m and len(weights) == m # make sure there are m trees in the forest
-
-def GBRT_test2():
-    m = 20
-    x = np.arange(100).reshape((100, 1))
-    y = np.arange(100)
-    max_depth = 4
-    trees, weights = GBRT(x, y, m, max_depth)
-    depths_forest = np.array([tree.depth for tree in trees]) # Get the depth of all trees in the forest
-    return np.all(depths_forest == max_depth) # make sure that the max depth of all the trees is correct
-
-def GBRT_test3():
-    m = 4
-    xTrSpiral,yTrSpiral,_,_= spiraldata(150)
-    max_depth = 4
-    trees, weights = GBRT(xTrSpiral, yTrSpiral, m, max_depth, 1) # Create a gradient boosted forest with 4 trees
-    
-    errs = [] 
-    for i in range(m):
-        predH = evalboostforest(trees[:i+1], xTrSpiral, weights[:i+1]) # calculate the prediction of the first i-th tree
-        err = np.mean(np.sign(predH) != yTrSpiral) # calculate the error of the first i-th tree
-        errs.append(err) # keep track of the error
-    
-    # your errs should be decreasing, i.e., the different between two subsequent errors is <= 0
-    return np.all(np.diff(errs) <= 0) 
-    
-runtest(GBRT_test1, 'GBRT_test1')
-runtest(GBRT_test2, 'GBRT_test2')
-runtest(GBRT_test3, 'GBRT_test3')
-
 trees, weights=GBRT(xTrSpiral,yTrSpiral, 40, maxdepth=4, alpha=0.03) # compute tree on training data 
 visclassifier(lambda X:evalboostforest(trees, X, weights),xTrSpiral,yTrSpiral)
 print("Training error: %.4f" % np.mean(np.sign(evalforest(trees,xTrSpiral)) != yTrSpiral))
@@ -285,206 +185,10 @@ plt.ylabel("error")
 plt.show()
 
 
-def onclick_forest(event):
-    """
-    Visualize forest, including new point
-    """
-    global xTrain,yTrain,w,b,M,Q,trees,weights
-    
-    if event.key == 'shift': Q+=10
-    else: Q+=1
-    Q=min(Q,M)
-​
-    classvals = np.unique(yTrain)
-        
-    # return 300 evenly spaced numbers over this interval
-    res=300
-    xrange = np.linspace(0, 1,res)
-    yrange = np.linspace(0, 1,res)
-    
-    # repeat this matrix 300 times for both axes
-    pixelX = repmat(xrange, res, 1)
-    pixelY = repmat(yrange, res, 1).T
-​
-    xTe = np.array([pixelX.flatten(), pixelY.flatten()]).T
-​
-    # get forest
-    
-    fun = lambda X:evalboostforest(trees[:Q],X, weights[:Q])        
-    # test all of these points on the grid
-    testpreds = fun(xTe)
-    trerr=np.mean(np.sign(fun(xTrain))==np.sign(yTrain))
-    
-    # reshape it back together to make our grid
-    Z = testpreds.reshape(res, res)
-    
-    plt.cla()    
-    plt.xlim((0,1))
-    plt.ylim((0,1))
-    # fill in the contours for these predictions
-    marker_symbols = ['o', 'x']
-    mycolors = [[0.5, 0.5, 1], [1, 0.5, 0.5]]
-    plt.contourf(pixelX, pixelY, np.sign(Z), colors=mycolors)
-    
-    for idx, c in enumerate(classvals):
-        plt.scatter(xTrain[yTrain == c,0],xTrain[yTrain == c,1],marker=marker_symbols[idx],color='k')
-    plt.show()
-    plt.title('# Trees: %i Training Accuracy: %2.2f' % (Q,trerr))
-    
-        
-xTrain=xTrSpiral.copy()/14+0.5
-yTrain=yTrSpiral.copy()
-yTrain=yTrain.astype(int)
-​
-# Hyper-parameters (feel free to play with them)
-M=50
-alpha=0.05;
-depth=5;
-trees, weights=GBRT(xTrain, yTrain, M,alpha=alpha,maxdepth=depth)
-Q=0;
-​
-​
-%matplotlib notebook
-fig = plt.figure()
-cid = fig.canvas.mpl_connect('button_press_event', onclick_forest) 
-print('Click to add a tree.');
-plt.title('Click to start boosting on the spiral data.')
-visclassifier(lambda X: np.sum(X,1)*0,xTrain,yTrain,newfig=False)
-plt.xlim(0,1)
-plt.ylim(0,1)
 
 
 
-def onclick_forest(event):
-    """
-    Visualize forest, including new point
-    """
-    global xTrain,yTrain,Q,trees,weights
-    
-    if event.key == 'shift': Q+=10
-    else: Q+=1
-    Q=min(Q,M)
 
 
-    plt.cla()    
-    plt.xlim((0,1))
-    plt.ylim((0,1))
-    pTest=evalboostforest(trees[:Q],xTest,weights[:Q])    
-    pTrain=evalboostforest(trees[:Q],xTrain,weights[:Q])    
-
-
-    errTrain=np.sqrt(np.mean((pTrain-yTrain)**2))
-    errTest=np.sqrt(np.mean((pTest-yTest)**2))
-
-    plt.plot(xTrain[:,0],yTrain,'bx')
-    plt.plot(xTest[:,0],pTest,'r-')
-    plt.plot(xTest[:,0],yTest,'k-')
-
-    plt.legend(['Training data','Prediction'])
-    plt.title('(%i Trees)  Error Tr: %2.4f, Te:%2.4f' % (Q,errTrain,errTest))
-    plt.show()
-    
-        
-n=100;
-NOISE=0.05
-xTrain=np.array([np.linspace(0,1,n),np.zeros(n)]).T
-yTrain=2*np.sin(xTrain[:,0]*3)*(xTrain[:,0]**2)
-yTrain+=np.random.randn(yTrain.size)*NOISE;
-ntest=300;
-xTest=np.array([linspace(0,1,ntest),np.zeros(ntest)]).T
-yTest=2*np.sin(xTest[:,0]*3)*(xTest[:,0]**2)
-
-
-
-    
-# Hyper-parameters (feel free to play with them)
-M=400
-alpha=0.05;
-depth=3;
-trees, weights=GBRT(xTrain, yTrain, M,alpha=alpha,maxdepth=depth)
-Q=0;
-
-%matplotlib notebook
-fig = plt.figure()
-cid = fig.canvas.mpl_connect('button_press_event', onclick_forest) 
-print('Click to add a tree (shift-click and add 10 trees).');
-plt.title('Click to start boosting on this 1D data (Shift-click to add 10 trees).')
-plt.plot(xTrain[:,0],yTrain,'*')
-plt.plot(xTest[:,0],yTest,'k-')
-plt.xlim(0,1)
-plt.ylim(0,1)
-
-
-def onclick_forest(event):
-    """
-    Visualize forest, including new point
-    """
-    global xTrain,yTrain,w,b,M
-    # create position vector for new point
-    pos=np.array([[event.xdata,event.ydata]]) 
-    if event.key == 'shift': # add positive point
-        color='or'
-        label=1
-    else: # add negative point
-        color='ob'
-        label=-1    
-    xTrain = np.concatenate((xTrain,pos), axis = 0)
-    yTrain = np.append(yTrain, label)
-    marker_symbols = ['o', 'x']
-    classvals = np.unique(yTrain)
-        
-    w = np.array(w).flatten()
-    
-    mycolors = [[0.5, 0.5, 1], [1, 0.5, 0.5]]
-    
-    # return 300 evenly spaced numbers over this interval
-    res=300
-    xrange = np.linspace(0, 1,res)
-    yrange = np.linspace(0, 1,res)
-    
-    # repeat this matrix 300 times for both axes
-    pixelX = repmat(xrange, res, 1)
-    pixelY = repmat(yrange, res, 1).T
-
-    xTe = np.array([pixelX.flatten(), pixelY.flatten()]).T
-
-    # get forest
-    trees, weights=GBRT(xTrain, yTrain, M)
-    fun = lambda X:evalboostforest(trees,X, weights)
-    # test all of these points on the grid
-    testpreds = fun(xTe)
-    
-    # reshape it back together to make our grid
-    Z = testpreds.reshape(res, res)
-    # Z[0,0] = 1 # optional: scale the colors correctly
-    
-    plt.cla()    
-    plt.xlim((0,1))
-    plt.ylim((0,1))
-    # fill in the contours for these predictions
-    plt.contourf(pixelX, pixelY, np.sign(Z), colors=mycolors)
-    
-    for idx, c in enumerate(classvals):
-        plt.scatter(xTrain[yTrain == c,0],
-            xTrain[yTrain == c,1],
-            marker=marker_symbols[idx],
-            color='k'
-            )
-    plt.show()
-    
-        
-xTrain= np.array([[5,6]])
-b=yTrIon
-yTrain = np.array([1])
-w=xTrIon
-M=5
-
-%matplotlib notebook
-fig = plt.figure()
-plt.xlim(0,1)
-plt.ylim(0,1)
-cid = fig.canvas.mpl_connect('button_press_event', onclick_forest)
-print('Note: You may notice a delay when adding points to the visualization.')
-plt.title('Use shift-click to add negative points.')
 
 
